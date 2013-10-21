d843167cce99dc298d1de890ed06cb81d777ab3c
Steven Futter <futter.steven@gmail.com>
2013-10-20 21:44:03 -0400

    updating test files for new nav dropdown. jquery slideDown will no longer push down proceeding divs

---
+++ js/javascript_test.js
@@ -0,0 +1,153 @@
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD LAYOUT WORK WHICH SLID THE DIVS DOWN ON HOVER OF NAV ELEMENTS 
+
+
+
+
+// MOUSEENTER AND MOUSELEAVE FOR LOVE NAVBAR
+
+$('nav ul #subNavOnLove li a').mouseenter(function(){
+    $('.subNavPortraits').stop(true).slideUp(350, function(){ 
+        $('.subNavEditorial').stop(true).slideUp(350, function(){ 
+            $('.subNavCommercial').stop(true).slideUp(350, function(){ 
+	           $('.subNavLove').stop(true).slideDown(350, function(){
+                });
+           });
+        });
+    });
+});
+
+
+
+$('nav ul #subNavOff li a').mouseenter(function(){
+	$('.subNavLove').stop(true).slideUp(350, function(){
+
+	});
+});
+
+
+$('.subNavOnLove').mouseenter(function(){
+	$(this).find(".subNavOnLove").stop().slideToggle();
+});
+
+// *****************END OF LOVE NAVBAR JQUERY*****************************
+
+
+// // MOUSEENTER AND MOUSELEAVE FOR PORTRAITS NAVBAR
+
+$('nav ul #subNavOnPortraits li a').mouseenter(function(){
+    $('.subNavLove').stop(true).slideUp(350, function(){ 
+        $('.subNavEditorial').stop(true).slideUp(350, function(){ 
+            $('.subNavCommercial').stop(true).slideUp(350, function(){ 
+               $('.subNavPortraits').stop(true).slideDown(350, function(){
+                });
+           });
+        });
+    });
+});
+
+
+
+$('nav ul #subNavOff li a').mouseenter(function(){
+    $('.subNavPortraits').stop(true).slideUp(350, function(){
+
+    });
+});
+
+
+$('.subNavOnPortraits').mouseenter(function(){
+    $(this).find(".subNavOnPortraits").stop().slideToggle();
+});
+
+
+// // *****************END OF PORTRAITS NAVBAR JQUERY************************
+
+
+// // MOUSEENTER AND MOUSELEAVE FOR EDITORIAL NAVBAR
+
+$('nav ul #subNavOnEditorial li a').mouseenter(function(){
+    $('.subNavLove').stop(true).slideUp(350, function(){ 
+        $('.subNavPortraits').stop(true).slideUp(350, function(){ 
+            $('.subNavCommercial').stop(true).slideUp(350, function(){ 
+               $('.subNavEditorial').stop(true).slideDown(350, function(){
+                });
+           });
+        });
+    });
+});
+
+
+
+$('nav ul #subNavOff li a').mouseenter(function(){
+    $('.subNavEditorial').stop(true).slideUp(350, function(){
+
+    });
+});
+
+
+$('.subNavOnEditorial').mouseenter(function(){
+    $(this).find(".subNavOnEditorial").stop().slideToggle();
+});
+
+
+// // *****************END OF EDITORIAL NAVBAR JQUERY************************
+
+
+// // MOUSEENTER AND MOUSELEAVE FOR COMMERCIAL NAVBAR
+
+$('nav ul #subNavOnCommercial li a').mouseenter(function(){
+    $('.subNavLove').stop(true).slideUp(350, function(){ 
+        $('.subNavPortraits').stop(true).slideUp(350, function(){ 
+            $('.subNavEditorial').stop(true).slideUp(350, function(){ 
+               $('.subNavCommercial').stop(true).slideDown(350, function(){
+                });
+           });
+        });
+    });
+});
+
+
+
+$('nav ul #subNavOff li a').mouseenter(function(){
+    $('.subNavEditorial').stop(true).slideUp(350, function(){
+
+    });
+});
+
+
+$('.subNavOnEditorial').mouseenter(function(){
+    $(this).find(".subNavOnEditorial").stop().slideToggle();
+});
+
+// *****************END OF COMMERCIAL NAVBAR JQUERY************************
+
+
+
+// MOUSEENTER AND MOUSELEAVE FOR OTHER PARTS OF WEBPAGE
+
+$('.mainPicture img').MOUSEENTER(function(){   
+    $('.subNavLove').stop(true).slideUp(350, function(){
+        $('.subNavPortraits').stop(true).slideUp(350, function(){
+            $('.subNavEditorial').stop(true).slideUp(350, function(){
+                $('.subNavCommercial').stop(true).slideUp(350, function(){
+                });
+            });
+        });
+    });
+});
+
+// *****************END OF 'OTHER PARTS' NAVBAR JQUERY*********************
+
+
+
